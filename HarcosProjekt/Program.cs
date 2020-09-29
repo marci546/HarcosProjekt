481bf9e5f0a418b60dbf0306526ca8b9dd2c5819
Diák <Dik@PETRIK>
2020-09-29 11:22:15 +0200

    nem akarok ide semmit se írni

---
+++ HarcosProjekt/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarcosProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("asd");
+            
+            Console.ReadLine();
+        }
+    }
+}
